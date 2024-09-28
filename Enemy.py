318882d145d0918cb0da46e2ed247cdf3af6ecb7
Arnav Pushkarna <kakamishu2012@gmail.com>
2024-09-28 15:27:21 -0700

    update and fix movement

--- Enemy.py
+++ Enemy.py
@@ -17,10 +17,13 @@ class Enemy:
     
     def handle_movement(self, target: tuple) -> None:
 
-        directionx = (self.x_pos - target[0])/(((self.x_pos**2) + (self.y_pos**2))**0.5)
-        directiony = (self.y_pos - target[1])/(((self.x_pos**2) + (self.y_pos**2))**0.5)
-        self.x_pos -= self.speed * directionx
-        self.y_pos -= self.speed * directiony
+        v = (self.x_pos - target(0), self.y_pos - target[1])
+        magnitude = (v[0]**2 + v[1]**2)**0.5
+        v[0] = v[0] / magnitude
+        v[1] = v[1] / magnitude
+
+        self.x_pos -= self.speed * v[0]
+        self.y_pos -= self.speed * v[1]
 
     def draw(self, screen: pygame.Surface, target: tuple):
         pygame.draw.rect(screen, pygame.Color(255, 0, 0), pygame.Rect(self.x_pos - (self.width/2), self.y_pos - (self.height/2), self.width, self.height))
@@ -41,11 +44,17 @@ class Triangle(Enemy):
     def draw(self, screen: pygame.Surface, center: tuple, target: tuple):
 
         centroid = (self.x_pos, self.y_pos)
-        point1 = ((centroid[0] - target[0]), (centroid[1] - target[1]))
+        distance = ((self.x_pos**2)+(self.y_pos**2))**0.5
+        vertexes = [None, None, None]
+        median = self.side*(3**0.5)/2
+        angle_to_origin = math.atan((centroid[1] - target[1])/(centroid[0] - target[0]))
+        # vertex1 = [centroid[0]-((median*(2/3))*math.)]
+
+        for i in range(3):
+            vertexes[i] = [centroid[0] - (median*(2/3)*math.cos(angle_to_origin + ((math.pi/3)*i))), centroid[1] + (median*(2/3)*math.cos(angle_to_origin + ((math.pi/3)*i)))]
         
 
-        pygame.draw.polygon(screen, pygame.Color(255, 0, 0) [vertex1, vertex2, vertex3])
-        pygame.draw.polygon()
+        pygame.draw.polygon(screen, pygame.Color(255, 0, 0), vertexes)
         
 
 
