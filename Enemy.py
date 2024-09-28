15407c5f17e7b1764daeed1b2fa9f8c78f66233d
Arnav Pushkarna <kakamishu2012@gmail.com>
2024-09-28 13:30:56 -0700

    Changed movement mechanics

--- Enemy.py
+++ Enemy.py
@@ -12,7 +12,9 @@ class Enemy:
         return f"Enemy [{self.x_pos}, {self.y_pos}, {self.speed}, {self.damage}, {self.width}, {self.height}]"
     
     def handle_movement(self) -> None:
-        self.x_pos -= self.speed
+
+        self.x_pos = self.speed * (self.x_pos)
+        self.y_pos = self.speed * (self.y_pos)
 
 class Triangle(Enemy):
 
@@ -34,6 +36,13 @@ class Rectangle(Enemy):
 
 
 class Pentagon(Enemy):
+
+    def __init__(self, speed: float, dmg: float, x: float, y: float, side: float) -> None:
+        self.speed = speed
+        self.damage = dmg
+        self.x_pos = x
+        self.y_pos = y
+        self.side = side
 
     def __str__(self) -> str:
         return f"Enemy [{self.x_pos}, {self.y_pos}, {self.speed}, {self.damage}]" 
