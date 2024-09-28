70d87337377699e8cb90f6802a2acbe62ad36df3
Arnav Pushkarna <kakamishu2012@gmail.com>
2024-09-28 13:13:13 -0700

    adding handle_movement

--- Enemy.py
+++ Enemy.py
@@ -1,14 +1,15 @@
 class Enemy:
 
-    def __init__(self, type: str, speed: float, dmg: float, x: float, y: float) -> None:
-        self.type = type
+    def __init__(self, speed: float, dmg: float, x: float, y: float, width: float, height: float) -> None:
         self.speed = speed
         self.damage = dmg
         self.x_pos = x
         self.y_pos = y
+        self.width = width
+        self.height = height
 
     def __str__(self) -> str:
-        return f"Enemy ['{type}', {self.speed}, {self.damage}]"
+        return f"Enemy [{self.x_pos}, {}, {self.speed}, {self.damage}]"
     
-    def handle_movement():
+    def handle_movement(self) -> None:
         pass
