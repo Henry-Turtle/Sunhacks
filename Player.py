ecb4e1305b04917ca9d7746eb36c641b2500bbca
Henry White <212whiteh@gmail.com>
2024-09-28 14:34:59 -0700

    h

--- Player.py
+++ Player.py
@@ -1,12 +1,17 @@
-
+import pygame
 class Player:
     def __init__(self):
 
         self.x = 50
         self.y = 50
+        self.speed = 5
 
-    def move(self):
-        pass
+    #*x and y should either be 1 or -1 to indicate direction
+    def move(self, x: int, y: int):
+        self.x += x * self.speed
+        self.y += y * self.speed
 
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.circle(screen, pygame.Color("green"), (self.x, self.y), 20)
 
 
