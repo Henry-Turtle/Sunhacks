46123830a759eecce04cdec2aad650cdb57e7451
Arnav Pushkarna <kakamishu2012@gmail.com>
2024-09-28 13:26:57 -0700

    Removed speed y

--- Enemy.py
+++ Enemy.py
@@ -1,8 +1,7 @@
 class Enemy:
 
-    def __init__(self, speedx: float, speedy: float, dmg: float, x: float, y: float, width: float, height: float) -> None:
+    def __init__(self, speedx: float, dmg: float, x: float, y: float, width: float, height: float) -> None:
         self.speed_x = speedx
-        self.speed_y = speedy
         self.damage = dmg
         self.x_pos = x
         self.y_pos = y
@@ -10,24 +9,22 @@ class Enemy:
         self.height = height
 
     def __str__(self) -> str:
-        return f"Enemy [{self.x_pos}, {self.y_pos}, {self.speed_x}, {self.speed_y}, {self.damage}, {self.width}, {self.height}]"
+        return f"Enemy [{self.x_pos}, {self.y_pos}, {self.speed_x}, {self.damage}, {self.width}, {self.height}]"
     
     def handle_movement(self) -> None:
-        self.x_pos -= self.speed_x
-        self.y_pos -= self.speed_y
+        self.x_pos -= self.speed
 
 class Triangle(Enemy):
 
-    def __init__(self, speedx: float, speedy: float, dmg: float, x: float, y: float, side: float) -> None:
+    def __init__(self, speedx: float, dmg: float, x: float, y: float, side: float) -> None:
         self.speed_x = speedx
-        self.speed_y = speedy
         self.damage = dmg
         self.x_pos = x
         self.y_pos = y
         self.side = side
 
     def __str__(self) -> str:
-        return f"Triangle [{self.x_pos}, {self.y_pos}, {self.speed_x}, {self.speed_y}, {self.damage}]"
+        return f"Triangle [{self.x_pos}, {self.y_pos}, {self.speed_x}, {self.damage}]"
 
 
 class Rectangle(Enemy):
@@ -39,4 +36,4 @@ class Rectangle(Enemy):
 class Pentagon(Enemy):
 
     def __str__(self) -> str:
-        return f"Enemy [{self.x_pos}, {self.y_pos}, {self.speed_x}, {self.speed_y}, {self.damage}]" 
+        return f"Enemy [{self.x_pos}, {self.y_pos}, {self.speed_x}, {self.damage}]" 
