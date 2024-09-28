c85ff819ae866470d3bcae5289f959f757d405fe
Arnav Pushkarna <kakamishu2012@gmail.com>
2024-09-28 13:23:10 -0700

    Added more enemy shapes

--- Enemy.py
+++ Enemy.py
@@ -1,7 +1,8 @@
 class Enemy:
 
-    def __init__(self, speed: float, dmg: float, x: float, y: float, width: float, height: float) -> None:
-        self.speed = speed
+    def __init__(self, speedx: float, speedy: float, dmg: float, x: float, y: float, width: float, height: float) -> None:
+        self.speed_x = speedx
+        self.speed_y = speedy
         self.damage = dmg
         self.x_pos = x
         self.y_pos = y
@@ -9,12 +10,33 @@ class Enemy:
         self.height = height
 
     def __str__(self) -> str:
-        return f"Enemy [{self.x_pos}, {self.y_pos}, {self.speed}, {self.damage}]"
+        return f"Enemy [{self.x_pos}, {self.y_pos}, {self.speed_x}, {self.speed_y}, {self.damage}, {self.width}, {self.height}]"
     
     def handle_movement(self) -> None:
-        pass
+        self.x_pos -= self.speed_x
+        self.y_pos -= self.speed_y
+
+class Triangle(Enemy):
+
+    def __init__(self, speedx: float, speedy: float, dmg: float, x: float, y: float, side: float) -> None:
+        self.speed_x = speedx
+        self.speed_y = speedy
+        self.damage = dmg
+        self.x_pos = x
+        self.y_pos = y
+        self.side = side
+
+    def __str__(self) -> str:
+        return f"Triangle [{self.x_pos}, {self.y_pos}, {self.speed_x}, {self.speed_y}, {self.damage}]"
+
+
+class Rectangle(Enemy):
+
+    def __str__(self) -> str:
+        return super().__str__()
+
 
 class Triangle(Enemy):
 
     def __str__(self) -> str:
-        return f""
+        return f"Enemy [{self.x_pos}, {self.y_pos}, {self.speed_x}, {self.speed_y}, {self.damage}]" 
