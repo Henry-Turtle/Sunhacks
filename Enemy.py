f1e183cabe0183aed57b7f1217780905e5e7c2a6
Arnav Pushkarna <kakamishu2012@gmail.com>
2024-09-28 20:45:12 -0700

    w

--- Enemy.py
+++ Enemy.py
@@ -64,44 +64,6 @@ class EnemySpiral(Enemy):
         super().__init__(speed, dmg, x, y, width, height, hp)
 
     def handle_movement(self, target: tuple) -> None:
-        # v = [self.x_pos - target[0], self.y_pos - target[1]]
-        # magnitude = (v[0]**2 + v[1]**2)**0.5
-        # v[0] = v[0] / magnitude
-        # v[1] = v[1] / magnitude
-
-        # if v[0] <= 0 and v[1] <= 0:
-        #     self.x_pos -= self.speed * (v[0] - 1)
-        #     self.y_pos -= self.speed * (v[1] + 0)
-        #     print("1")
-        # elif v[0] >= 0 and v[1] <= 0:
-        #     self.x_pos -= self.speed * (v[0] - 1)
-        #     self.y_pos -= self.speed * (v[1] - 0.1)
-        #     print("2")
-        # elif v[0] >= 0 and v[1] >= 0:
-        #     self.x_pos -= self.speed * (v[0] + 1)
-        #     self.y_pos -= self.speed * (v[1] - 1)
-        #     print("3")
-        # elif v[0] <= 0 and v[1] >= 0:
-        #     self.x_pos -= self.speed * (v[0] - 1)
-        #     self.y_pos -= self.speed * (v[1] + 1)
-        #     print("4")
-
-        '''actual_x = self.x_pos - target[0]
-        actual_y = - self.y_pos + target[1]
-
-        radius = ((actual_x**2) + (actual_y**2))**0.5
-        angle = math.atan2(actual_y, actual_x)
-
-        angle -= 0.001
-        radius -= 0.1
-
-        actual_y = radius*math.sin(radius)
-        actual_x = radius*math.cos(radius)
-
-        print(actual_x, actual_y)
-
-        self.x_pos = target[0] + actual_x
-        self.y_pos = target[1] - actual_y'''
 
         actual_x = self.x_pos - target[0]
         actual_y = self.y_pos - target[1]
@@ -111,8 +73,8 @@ class EnemySpiral(Enemy):
         angle = math.atan2(actual_y, actual_x)
 
         # Update the angle and radius to create spiral movement
-        angle -= 0.02  # Slightly change the angle for spiral effect
-        radius -= 2  # Decrease radius to move inward
+        angle -= 0.005 * self.speed  # Slightly change the angle for spiral effect
+        radius -= 0.5*self.speed  # Decrease radius to move inward
 
         # Convert polar coordinates back to cartesian coordinates
         new_x = radius * math.cos(angle)
